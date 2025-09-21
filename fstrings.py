937f60c526fc171b2bf4239a12cb574448e35143
Tanushree Borase <tanushreebcs@gmail.com>
2025-09-21 09:33:55 +0530

    Create fstrings.py

---
+++ fstrings.py
@@ -0,0 +1,10 @@
+name: "Tanushree"
+age: 21
+
+print(f"My name is{name} and I am {age} years old.") # My name is Tanushree and I am 21 years old.
+
+print(f"Product of mine and my marks in test is {21*50}") # Product of mine and my marks in test is 1050
+
+
+pi = 3.14
+print(f"Area of circle is {pi* 5*5:.2f}") # Area of circle is 78.50 (ans till 2 decimal places)
