52774f9c1c796afef46bc0f06c12e80407c793cc
Tanushree Borase <tanushreebcs@gmail.com>
2025-09-21 08:52:57 +0530

    Create firstProgram.py

---
+++ firstProgram.py
@@ -0,0 +1,13 @@
+
+name = input("What is the name of your true coach? ")
+
+print("Thank you " + name + " for being my guiding light!")
+
+'''
+Sample Input
+Ramesh Sir
+
+Your Output
+What is the name of your true coach? Ramesh Sir
+Thank you Ramesh Sir for being my guiding light!
+'''
