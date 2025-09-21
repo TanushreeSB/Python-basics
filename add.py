a7337db37aa3ce85ff01d7862523b4804f054334
Tanushree Borase <tanushreebcs@gmail.com>
2025-09-21 09:01:06 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,16 @@
+x=10
+y=20
+print(x+y)
+
+length = 20
+breadth = 10
+
+area = length * breadth;
+print("Area of rectangle: ", area) # Use commas in print (auto-converts types), eg: to join string and an integer
+
+'''
+output:
+
+30
+Area of rectangle:  200
+'''
