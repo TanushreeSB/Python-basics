b33899b6b7baa296b1fa5eae509e7fd61f89a550
Tanushree Borase <tanushreebcs@gmail.com>
2025-09-23 07:58:41 +0530

    Create secondProgram.py

---
+++ secondProgram.py
@@ -0,0 +1,29 @@
+name = "Tanushree"
+print("My name is: ", name);
+
+a = 10
+b = 20
+
+sum_result = a + b
+division_result = a / b
+
+print("Sum: ",sum_result)
+print("Division: ",division_result)
+
+greeting = "Good morning"
+name = "Tanushree"
+
+message = greeting + " " + name + "!"
+message1 = greeting,name # Outside print(), commas create a tuple.
+print(message)
+print(message1)
+
+'''
+output:
+
+My name is:  Tanushree
+Sum:  30
+Division:  0.5
+Good morning Tanushree!
+('Good morning', 'Tanushree')
+'''
